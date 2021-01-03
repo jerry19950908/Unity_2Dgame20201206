12a821b133c4b0ee9ed97603e406641e7d2382ee
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2021-01-03 13:01:23 +0800

    非靜態API介紹與場景按鈕

---
+++ Unity_2DWarrior/Assets/Scripts/API.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class API : MonoBehaviour
+{
+
+    //修飾詞 類別 欄位;
+    public Transform Tra;
+    public Transform tester;
+
+    public Camera cam;
+
+    public SpriteRenderer testerspr;
+    public GameObject tester2;
+
+
+    private void Start()
+    {
+        //非靜態屬性
+        //取得: 欄位.屬性
+        print("座標" + Tra.position);
+
+        //設定: 欄位.屬性 = 值
+        tester.position = new Vector3(2, 0, 0);
+
+
+        //靜態屬性
+        print("攝影機數量:" + Camera.allCamerasCount);
+        //非靜態屬性(要先設定欄位)
+        cam.backgroundColor = new Color(0.5f, 0.2f, 0.3f);
+
+
+        //練習
+        print("圖片顏色:" + testerspr.color);
+        print("圖片圖層:" + tester2.layer);
+        testerspr.flipX = true;
+        tester2.layer = 5; 
+    }
+
+
+    private void Update()
+    {
+        //非靜態方法
+        //欄位.方法(對應參數)
+        //變形欄位.旋轉(x,y,z)
+        tester.Rotate(0, 0, 30);
+
+        //位移(x,y,z,空間)
+        //空間有兩種: World世界, Self區域
+        tester.Translate(0.5f, 0, 0, Space.World);
+
+    }
+
+
+
+
+
+
+}
---
+++ Unity_2DWarrior/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement; //引用場景管理API
+using UnityEngine;
+
+public class SceneControl : MonoBehaviour
+{
+    //按鈕要呼叫方法，需把方法公開 public
+
+  public void Startgame()
+    { 
+        //要先把製作好的場景放在 file - buildsettings...裡面
+        //場景管理器.載入場景("遊戲場景");
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+  public void BacktoMenu()
+    {
+        //場景管理器.載入場景("選單");
+        SceneManager.LoadScene("選單");
+    }
+
+
+    /// <summary>
+    /// 離開遊戲方法
+    /// </summary>
+    public void QuitGame()
+    {
+        //應用程式.離開();
+        Application.Quit();
+    }
+}
